0c2863775b7ec2cbc03b99f2d1d3295422aa5d73
tepepe61 <akimiatepp61@gmail.com>
2023-01-18 22:13:49 +0900

    firestoreにtodoの内容を保存できるように接続した

--- reactApp/react-todo-pwa/src/components/Dashboard.js
+++ reactApp/react-todo-pwa/src/components/Dashboard.js
@@ -1,24 +1,32 @@
-import React, {userState, useEffect, useContext} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import { signInWithGoogle } from '../service/firebase';
 import dig from 'object-dig';
 import {AuthContext} from "../provider/AuthProvider";
+import * as Api from '../service/api'
 
 const Dashboard = () => {
   const currentUser = useContext(AuthContext);
+  const [inputName, setInputName] = useState("");
+  // console.log(inputName);
 
   const formRender = () => {
     let dom
     if(dig(currentUser, 'currentUser', 'uid')){
       dom =
         <form>
-          <input placeholder="ToDoName"/>
-          <button></button>
+          <input placeholder="ToDoName" value={inputName} onChange={(event) => setInputName(event.currentTarget.value)} />
+          <button type='button' onClick={() => poost()}>追加</button>
         </form>
     }else{
       dom = <button onClick={signInWithGoogle}>ログイン</button>
     }
 
     return dom
+  }
+
+  const poost = () => {
+    Api.addTodo(inputName, currentUser.currentUser.uid);
+    setInputName("");
   }
 
   return(
