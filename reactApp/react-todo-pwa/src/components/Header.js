94b09dc73a18132bf016b398429985544ff4bc6f
tepepe61 <akimiatepp61@gmail.com>
2023-01-19 22:56:46 +0900

    Headerにmaterial-uiを適用した

--- reactApp/react-todo-pwa/src/components/Header.js
+++ reactApp/react-todo-pwa/src/components/Header.js
@@ -2,28 +2,46 @@ import React, {useContext, useState} from "react";
 import dig from 'object-dig';
 import {signInWithGoogle, logOut} from "../service/firebase";
 import { AuthContext } from "../provider/AuthProvider";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import {makeStyles} from "@material-ui/core/styles";
 
+const useStyles = makeStyles(() => ({
+  toolbar: {
+    justifyContent: 'space-between'
+  },
+  button: {
+    color: "#FFF"
+  }
+}));
 
 const Header = () => {
   // useContextはReact.createContext();で生成したstateを引数でもらって使えるようにしてる。
   const currentUser = useContext(AuthContext);
-  console.log(currentUser)
+  const classes = useStyles();
 
   const buttonRender = () => {
     let buttonDom
+
     if(dig(currentUser, 'currentUser', 'uid')){
-      buttonDom = <button onClick={logOut} >ログアウト</button>
+      buttonDom = <Button className={classes.button} variant='inherit' onClick={logOut} >ログアウト</Button>
     }else{
-      buttonDom = <button onClick={signInWithGoogle} >ログイン</button>
+      buttonDom = <Button className={classes.button} variant='inherit' onClick={signInWithGoogle} >ログイン</Button>
     }
     return buttonDom
   }
 
   return(
-    <header>
-      へっだー
-      { buttonRender() }
-    </header>
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h6">
+          ReactToDo
+        </Typography>
+        {buttonRender()}
+      </Toolbar>
+    </AppBar>
   )
 }
 
