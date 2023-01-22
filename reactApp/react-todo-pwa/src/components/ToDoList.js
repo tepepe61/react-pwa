7312a4983f6faa9ac9b0304ac3c30028350f8142
tepepe61 <akimiatepp61@gmail.com>
2023-01-23 02:34:07 +0900

    checkboxと削除アイコンを生成

--- reactApp/react-todo-pwa/src/components/ToDoList.js
+++ reactApp/react-todo-pwa/src/components/ToDoList.js
@@ -3,29 +3,52 @@ import * as Api from "../service/api";
 import { signInWithGoogle } from '../service/firebase';
 import dig from 'object-dig';
 import { AuthContext } from '../provider/AuthProvider';
+import {ListItem, ListItemIcon, ListItemText, ListItemSecondaryAction, IconButton, Checkbox} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import {makeStyles} from '@material-ui/core';
 
+
+const useStyle = makeStyles(() => ({
+  root: {
+    maxWidth: 360,
+    margin: 'auto',
+  },
+  ul: {
+    paddingLeft: 0,
+    listStyle: 'none',
+  },
+  list: {
+    justifyContent: 'space-btween'
+  }
+}));
 
 const ToDoList = (props) =>{
+  const classes = useStyle();
   const deleteHandle = async(id) => {
     await Api.todoDelete(id);
     props.fetch();
   }
 
   const todoList = props.todos.map((todo) => {
-    return(
-      <li key={todo.id}>
-        {todo.content}
-        <button type="button" onClick={() => deleteHandle(todo.id)}>
-          削除
-        </button>
-      </li>
+    return (
+      <ListItem key={todo.id}>
+        <ListItemIcon>
+          <Checkbox name="checkedA" />
+        </ListItemIcon>
+        <ListItemText primary={todo.content} />
+        <ListItemSecondaryAction>
+          <IconButton edge="end" aria-label="delete" onClick={() => deleteHandle(todo.id)}>
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
     );
   });
 
   return(
-    <div>
+    <div className={classes.root}>
       <h2>あなたのToDo</h2>
-      <ul>{todoList}</ul>
+      <ul className={classes.ul}>{todoList}</ul>
     </div>
   )
 }
